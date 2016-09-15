89053e2cb6d1f201bb3b6ec6139b19ad77b61a61
lehaisonmath6 <lehaisonmath6@gmail.com>
2016-09-15 23:25:25 +0700

    Đã thêm load dữ liệu
    Sẽ tiếp tục cập nhật thêm

--- QuanLiNhanSu/frmMain.cs
+++ QuanLiNhanSu/frmMain.cs
@@ -6,7 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using DAO;
+using DTO;
 namespace QuanLiNhanSu
 {
     public partial class frmMain : Form
@@ -19,6 +20,24 @@ namespace QuanLiNhanSu
         private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            dgvNhanVien.DataSource = NhanVienDAO.LoadDataNV();
+            SetHeaderColumn();
+        }
+        public void SetHeaderColumn()
+        {
+            dgvNhanVien.Columns["IDNhanVien"].HeaderText = "Mã nhân viên";
+            dgvNhanVien.Columns["HoTen"].HeaderText = "Họ và tên";
+            dgvNhanVien.Columns["NgaySinh"].HeaderText = "Ngày sinh";
+            dgvNhanVien.Columns["GioiTinh"].HeaderText = "Giới tính";
+            dgvNhanVien.Columns["QueQuan"].HeaderText = "Quê quán";
+            dgvNhanVien.Columns["ChucVu"].HeaderText = "Chức vụ";
+            dgvNhanVien.Columns["QuanLi"].HeaderText = "Quản lí";
+            dgvNhanVien.Columns["Luong"].HeaderText = "Lương";
+            dgvNhanVien.Columns["TenPhong"].HeaderText = "Tên phòng";
         }
     }
 }
